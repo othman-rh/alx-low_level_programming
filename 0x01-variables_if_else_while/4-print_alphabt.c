1ebbb71ab31c7da3a160429acd7acfd96465d473
Othman Rhajbal <othman.rhajbal00@gmail.com>
2023-10-06 18:40:01 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,12 +3,10 @@
 #include <string.h>
 
 /**
- * main - Prints the alphabet without q and e
- * 
+ * main - Prints the alphabet without q and e.
+ *
  * Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
 	int i;
@@ -23,3 +21,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
