23f6437b0d3c02e4b18b86371370ce049ea3088a
Othman Rhajbal <othman.rhajbal00@gmail.com>
2023-10-06 18:34:32 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+
 int main(void)
 {
 	int i;
