abe703dab02574f8d77ff04c632abae1c447261d
Othman Rhajbal <othman.rhajbal00@gmail.com>
2023-10-31 18:06:25 -0700

    0x0B malloc free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,47 +1,71 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * argstostr - function concatenates all the arguments of your program.
- * @ac: pointer
- * @av: altura
+ * _strcat - Concat string, src appended to dest
  *
- * REturn: result
+ * @dest: Destination string
+ * @src: Source string
+ * Return: Concat newString to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+        int a, b;
+
+        for (a = 0; dest[a] != '\0'; a++)
+                ;
+        for (b = 0; src[b] != '\0'; b++)
+        {
+                dest[a] = src[b];
+                a++;
+        }
+        return (dest);
+}
+
+/**
+ * _strlen - Return the number of the length of the string
+ *
+ * @s: This is the string to check
+ *
+ * Return: Return the length of the string
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+	return (a);
+}
+
+/**
+ * argstostr - Concats argv to new string
+ *
+ * @ac: Argument count
+ * @av: Arguments
+ *
+ * Return: Returns a pointer to a new string, or NULL if it fails
  */
 
 char *argstostr(int ac, char **av)
 {
-	int i = 0, j = 0, cont = 0;
-	char *result, *new;
+	int index, sumString;
+	char *newString;
+
+	sumString = 1;
 
 	if (ac == 0 || av == NULL)
+		return (0);
+	for (index = 0; index < ac && av[index]; index++)
+	{
+		sumString += _strlen(av[index]);
+	}
+	newString = malloc((sumString * sizeof(char)) + ac);
+	for (index = 0; index < ac && av[index]; index++)
+	{
+		newString = _strcat(newString, av[index]);
+		newString = _strcat(newString, "\n");
+	}
+	if (newString == NULL)
 		return (NULL);
-	while (i < ac)
-	{
-		new = av[i];
-		while (*new)
-		{
-			cont++;
-			new++;
-		}
-		cont++;
-		i++;
-	}
-	result = malloc(cont + 1);
-	i = 0;
-	while (i < ac)
-	{
-		new = av[i];
-		while (*new)
-		{
-			result[j] = *new;
-			new++;
-			j++;
-		}
-		result[j] = '\n';
-		j++;
-		i++;
-	}
-	result[cont + 1] = '\0';
-	return (result);
+	return (newString);
 }
