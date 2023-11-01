8c7efead4b20d2cf7a3f960715789ab8de6baf0e
Othman Rhajbal <othman.rhajbal00@gmail.com>
2023-10-31 18:14:19 -0700

    0x0B malloc free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,71 +1,46 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _strcat - Concat string, src appended to dest
- *
- * @dest: Destination string
- * @src: Source string
- * Return: Concat newString to dest
- */
-char *_strcat(char *dest, char *src)
-{
-        int a, b;
-
-        for (a = 0; dest[a] != '\0'; a++)
-                ;
-        for (b = 0; src[b] != '\0'; b++)
-        {
-                dest[a] = src[b];
-                a++;
-        }
-        return (dest);
-}
-
-/**
- * _strlen - Return the number of the length of the string
- *
- * @s: This is the string to check
- *
- * Return: Return the length of the string
- */
-int _strlen(char *s)
-{
-	int a;
-
-	for (a = 0; s[a] != '\0'; a++)
-		;
-	return (a);
-}
-
-/**
- * argstostr - Concats argv to new string
- *
- * @ac: Argument count
- * @av: Arguments
- *
- * Return: Returns a pointer to a new string, or NULL if it fails
- */
+* *argstostr - function that concatenates all the arguments of your program.
+* @ac : argument of counter
+;* @av : argument of vector
+* Return: pointer
+*/
 
 char *argstostr(int ac, char **av)
 {
-	int index, sumString;
-	char *newString;
-
-	sumString = 1;
+	int c, c1;
+	int a = 0;
+	int str = 0;
+	char *d;
 
 	if (ac == 0 || av == NULL)
-		return (0);
-	for (index = 0; index < ac && av[index]; index++)
-	{
-		sumString += _strlen(av[index]);
-	}
-	newString = malloc((sumString * sizeof(char)) + ac);
-	for (index = 0; index < ac && av[index]; index++)
-	{
-		newString = _strcat(newString, av[index]);
-		newString = _strcat(newString, "\n");
-	}
-	if (newString == NULL)
 		return (NULL);
-	return (newString);
+
+	for (c = 0 ; c < ac ; c++)
+	{
+		for (c1 = 0 ; av[c][c1] != '\0' ; c1++)
+		{
+			str++;
+		}
+		str++;
+	}
+
+	d = malloc(sizeof(char) * str);
+
+	if (d == NULL)
+		return (NULL);
+
+	for (c = 0 ; c < ac ; c++)
+	{
+		for (c1 = 0 ; av[c][c1] != '\0' ; c1++)
+		{
+			d[a] = av[c][c1];
+			a++;
+		}
+		d[a] = '\n';
+		a++;
+	}
+	return (d);
 }
