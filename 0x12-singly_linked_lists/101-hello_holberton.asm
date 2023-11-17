006c627a858270bdcc70ac97130529a5f4134139
Othman Rhajbal <othman.rhajbal00@gmail.com>
2023-11-16 17:06:36 -0800

    0x12 singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,21 @@
-Creating files
+	extern printf
+
+	section .data
+message:	db "Hello, Holberton", 0
+format:		db "%s", 10, 0
+
+	section .text
+
+	global main
+main:
+	push	rbp
+
+	mov 	rax, 0
+	mov	rdi,format
+	mov	rsi,message
+	call	printf
+
+	pop	rbp
+
+	mov 	rax, 0
+	ret
