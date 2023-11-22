edd245f52b6b135d6b908d02e786866a0de17496
Othman Rhajbal <othman.rhajbal00@gmail.com>
2023-11-21 18:56:03 -0800

    More Singly Linked Lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,21 @@
 #include "lists.h"
-
 /**
- * free_listint - Free listint_t.
- * @head: data type pointer the head/next node
- * Return: 0 elements all free
+ * free_listint - Free nodes and list
+ * @head: This is my struct
  */
 void free_listint(listint_t *head)
 {
+
+	if (head == NULL)
+	{
+		return;
+	}
 	while (head != NULL)
 	{
-		free(head);
+		listint_t *tmp;
+
+		tmp = head;
+		free(tmp);
 		head = head->next;
 	}
 }
