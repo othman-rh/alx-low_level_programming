fe55838e5b4c1406bb2d7f7b2a4bc0defed08252
Othman Rhajbal <othman.rhajbal00@gmail.com>
2023-11-27 20:25:01 -0800

    0x15 C File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,28 +1,32 @@
 #include "main.h"
-
 /**
- * append_text_to_file - Function that creates a file
- * @filename: type char filename pointer of FD
- * @text_content: type char pointer of char
- * Return: -1 if NULL in FN, 1 on sucess
+ * append_text_to_file - That appends text at the end of a file.
+ * @filename: THis is the name of the file
+ * @text_content: This the string to append to text
+ *
+ * Return: 1 if is success and if not is -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file;
-	int final_out;
-	int x;
+	int fd;
 
-	file = open(filename, O_RDWR | O_APPEND);
-	if (filename == NULL)
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
 		return (-1);
-	if (file == -1)
-	return (-1);
-	if (text_content == NULL)
-		return (1);
-	while (*(text_content + x) != '\0')
-		x++;
-	final_out = write(file, text_content, x);
-	if (final_out == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		int len_Text, lenWrite;
+
+		for (len_Text = 0; text_content[len_Text] != '\0'; len_Text++)
+			;
+		lenWrite = write(fd, text_content, len_Text);
+
+		if (lenWrite == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
 	return (1);
 }
